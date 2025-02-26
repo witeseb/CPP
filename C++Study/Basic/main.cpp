8e9eb2462dc8ca9085538c98ee8668580d216c70
witeseb.t@outlook.com <tanatanee.wbst@mail.kmutt.ac.th>
2025-02-27 04:03:20 +0700

    Create main.cpp to run other files.

---
+++ C++Study/Basic/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// Declare the functions defined in other files
+void InputAndOperators(void);
+void OutputAndVariables(void);
+
+int main() {
+    std::cout << "Game Engine Starting...\n";
+
+    // OutputAndVariables();
+    InputAndOperators();
+
+    return 0;
+}
